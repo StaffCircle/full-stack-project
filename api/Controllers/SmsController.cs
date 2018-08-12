890149f1a79a3ba4d35305f9e1e9eb6a720ad01b
Luke Miles <lukemiles@outlook.com>
2018-08-12 17:54:09 +0100

    Initial project template creation

---
+++ api/Controllers/SmsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(new string[] { "value1", "value2" });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return this.Ok();
+        }
+    }
+}
---
+++ tests/api.tests/Controllers/SmsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace api.tests.Controllers
+{
+    public class SmsControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsActionResult_WithCollectionOfStrings()
+        {
+            var smsController = new SmsController();
+            var result = smsController.Get();
+
+            // Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<string>>(
+                viewResult.Value);
+            Assert.Equal(2, model.Count());
+        }
+
+        [Fact]
+        public void Post_ReturnsActionResult_With200Status()
+        {
+            var smsController = new SmsController();
+            var result = smsController.Post("");
+
+            // Assert
+            var viewResult = Assert.IsType<OkResult>(result);
+            Assert.Equal(200, viewResult.StatusCode);
+        }
+    }
+}
